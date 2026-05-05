9eb114ab314adc9fbb5434f035897d18a62fd506
Luythen <andenklasson@gmail.com>
2026-05-05 20:09:47 +0200

    add/Home.tsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div className="container-sm">
+            <div className="text-center mt-5">
+                <h1>For you timetrackering needs</h1>
+                <a className="btn btn-primary" href="/login">Get started now!</a>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
